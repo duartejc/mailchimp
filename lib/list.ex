d46896cf276579618afb9b6ad46100f78b77bb25
Rawane Zossou <dev@raw1z.fr>
2015-09-19 03:57:04 +0200

    refactor the list members related apis

--- lib/list.ex
+++ lib/list.ex
@@ -1,6 +1,8 @@
 defmodule Mailchimp.List do
+  alias HTTPoison.Response
   alias Mailchimp.HTTPClient
   alias Mailchimp.Link
+  alias Mailchimp.Member
 
   defstruct id: nil, name: nil, contact: nil, permission_reminder: nil, use_archive_bar: nil, campaign_defaults: nil, notify_on_subscribe: nil, notify_on_unsubscribe: nil, list_rating: nil, email_type_option: nil, subscribe_url_short: nil, subscribe_url_long: nil, beamer_address: nil, visibility: nil, modules: [], stats: nil, links: []
 
@@ -24,5 +26,38 @@ defmodule Mailchimp.List do
       stats: attributes[:stats],
       links: Link.get_links_from_attributes(attributes)
     }
+  end
+
+  def members(%__MODULE__{links: %{"members" => %Link{href: href}}}) do
+    {:ok, response} = HTTPClient.get(href)
+    case response do
+      %Response{status_code: 200, body: body} ->
+        {:ok, Enum.map(body.members, &Member.new(&1))}
+
+      %Response{status_code: _, body: body} ->
+        {:error, body}
+    end
+  end
+
+  def create_member(%__MODULE__{links: %{"members" => %Link{href: href}}}, email_address, status, merge_fields \\ %{}) when is_binary(email_address) and is_map(merge_fields) and status in [:subscribed, :pending, :unsubscribed, :cleaned] do
+    {:ok, response} = HTTPClient.get(href)
+    case response do
+      %Response{status_code: 200, body: body} ->
+        links = Link.get_links_from_attributes(body)
+        href = links["create"].href
+        data = %{email_address: email_address, status: status, merge_fields: merge_fields}
+
+        {:ok, response} = HTTPClient.post href, Poison.encode!(data)
+        case response do
+          %Response{status_code: 200, body: body} ->
+            {:ok, Member.new(body)}
+
+          %Response{status_code: _, body: body} ->
+            {:error, body}
+        end
+
+      %Response{status_code: _, body: body} ->
+        {:error, body}
+    end
   end
 end
